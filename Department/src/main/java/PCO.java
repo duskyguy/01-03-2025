7a784f9eac5cde2e54112b5ba53937fc9e259dbd
Nitish Gupta <36029518+imnitishgithub@users.noreply.github.com>
2018-02-23 00:25:13 +0530

    Add files via upload

---
+++ Department/src/main/java/PCO.java
@@ -0,0 +1,13 @@
+
+public class PCO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Jenkins Environment !!!");
+		System.out.println("                                   ");
+		System.out.println("Our depatment name is Premium Cloud Operations.");
+		System.out.println("                                   ");
+		System.out.println("We are from EM Monitoring Support Team.");
+	}
+
+}
